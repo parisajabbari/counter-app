03df48b1faa01377be714d4617492b8da7ec9c27
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-06-15 16:26:50 +1000

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = { count: 1 };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
+}
+
+export default Counter;

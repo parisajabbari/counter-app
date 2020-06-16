77e05cad260a1a6dc511bdc566d78e4936442805
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-06-16 14:28:03 +1000

    property name changed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -28,13 +28,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
   }
 }
 

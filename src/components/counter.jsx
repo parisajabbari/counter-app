6c175896a1a70d8aa40490e21ef7ffdf86e1bad5
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-06-18 13:02:38 +1000

    create single source of truth by reading all data from props not local state. updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { value: this.props.counter.value };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
@@ -17,7 +11,8 @@ class Counter extends Component {
         */}
 
         <button
-          onClick={this.handleIncrement}
+          /*pass the whole counter object instead of id*/
+          onClick={() => this.props.OnIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -35,12 +30,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "zero" : value;
   }
 }
